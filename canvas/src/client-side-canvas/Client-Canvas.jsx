9521273fd44a723d3fdcf64ab0af5048593ae465
anirban <anirbangoswami323@gmail.com>
2023-10-11 23:07:27 +0530

    hh

--- canvas/src/client-side-canvas/Client-Canvas.jsx
+++ canvas/src/client-side-canvas/Client-Canvas.jsx
@@ -4,67 +4,7 @@ import io from 'socket.io-client';
 function Clientcanvas() {
   const canvasRef = useRef(null);
   const contextRef = useRef(null);
-  const socket = io('http://localhost:9000/');
-
-  // useEffect(() => {
-  //   const canvas = canvasRef.current;
-  //   const context = canvas.getContext('2d');
-  //   context.fillStyle = 'white';
-  //   context.fillRect(0, 0, canvas.width, canvas.height);
-
-  //   // Store the initial point for drawing
-  //   let lastPoint = null;
-  //   let isDrawing = true; // Add this flag to control drawing
-
-
-     
-  //     socket.on('broadcast-points', (e) => {
-  //       if (e.type === 'start') {
-  //         isDrawing = true;
-  //         lastPoint = { x: e.x, y: e.y }; // Store the starting point
-  //         return;
-  //       } else if (e.type === 'stop') {
-  //         isDrawing = false;
-  //         lastPoint = null;
-  //         return;
-  //       }
-      
-  //       if (!isDrawing) return;
-      
-  //       const canvasX = e.x; // Use the raw X coordinate sent from the server
-  //       const canvasY = e.y; // Use the raw Y coordinate sent from the server
-      
-  //       if (lastPoint && isWithinCanvas(canvasX, canvasY, canvas)) {
-  //         const { x: startX, y: startY } = lastPoint;
-      
-  //         context.strokeStyle = 'black'; // Line color
-  //         context.lineWidth = 2; // Line width
-  //         context.lineCap = 'round';
-      
-  //         context.beginPath();
-  //         context.moveTo(startX, startY); // Start from
-  //         context.lineTo(canvasX, canvasY); // Draw to
-  //         context.stroke();
-  //         lastPoint = { x: canvasX, y: canvasY };
-  //       } else if (isWithinCanvas(canvasX, canvasY, canvas)) {
-  //         lastPoint = { x: canvasX, y: canvasY };
-  //       }
-  //     });
-
-
-  //   socket.on('stop-points', () => {
-  //     isDrawing = false;
-  //     lastPoint=null
-  //   });
-  //   socket.on('start-points', () => {
-  //     isDrawing = true;
-  //     lastPoint=null
-  //   });
-  // }, []);
-
-
-
-
+  const socket = io('https://scribble-clone.vercel.app/')
   useEffect(() => {
     const canvas = canvasRef.current;
     const context = canvas.getContext('2d');
